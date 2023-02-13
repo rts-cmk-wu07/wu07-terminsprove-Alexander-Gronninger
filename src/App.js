f9080da866358ac150ab102e3ace626304efcaf7
Alexander-Gronninger <alexander.gronninger@gmail.com>
2023-02-13 10:17:13 +0100

    create welcome.js and add images

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Welcome from "./pages/Welcome";
+
+const App = () => {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,16 @@
+const Welcome = () => {
+  return (
+    <>
+      <section className="grid">
+        <div>
+          <img src="./images/welcome_background.jpg" alt="" />
+        </div>
+        <div>
+          <img src="./images/welcome_center.jpg" alt="" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Welcome;
