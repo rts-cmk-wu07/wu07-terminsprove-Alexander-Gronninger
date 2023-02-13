f54fd3b7ed053b0361f993c13f63a9b73000f82b
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2023-02-13 13:40:15 +0100

    welcome page done - with polish still to do

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Welcome = () => {
   return (
     <>
-      <section className="flex items-end h-screen bg-[url('./backgrounds/welcome_background.jpg')] bg-cover bg-no-repeat bg-center">
-        <div className="w-full h-[400px] bg-[url('./backgrounds/welcome_center.jpg')] bg-cover bg-no-repeat bg-center mb-14"></div>
+      <section className="flex flex-col justify-end h-screen bg-[url('./backgrounds/welcome_background.jpg')] bg-cover bg-no-repeat bg-top">
+        <div className="mb-10">
+          <h1 className="text-white text-veryBig mb-4 ml-10">
+            Believe Yourself
+          </h1>
+          <div className="flex">
+            <div className="w-11 mr-2 border-[1px] h-0 self-center"></div>
+            <p className="text-white text-normal">Train like a pro</p>
+          </div>
+        </div>
+        <div className="grid">
+          <div className="flex w-full h-[400px] bg-[url('./backgrounds/welcome_center.jpg')] bg-cover bg-no-repeat bg-center mb-14 col-start-1 col-end-1 row-start-1 row-end-3"></div>
+          <Link
+            to="home"
+            className="bg-white self-end ml-auto py-4 px-[65px] col-start-1 col-end-1 row-start-1 row-end-4 mb-[15px] text-normal rounded-l-2xl"
+          >
+            Start Training
+          </Link>
+        </div>
       </section>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,40 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
+      fontSize: {
+        small: [
+          "22px",
+          {
+            lineHeight: "26px",
+            letterSpacing: "-0.01em",
+            fontWeight: "400",
+          },
+        ],
+        normal: [
+          "28px",
+          {
+            lineHeight: "32px",
+            letterSpacing: "-0.01em",
+            fontWeight: "400",
+          },
+        ],
+        big: [
+          "50px",
+          {
+            lineHeight: "54px",
+            letterSpacing: "-0.01em",
+            fontWeight: "400",
+          },
+        ],
+        veryBig: [
+          "62px",
+          {
+            lineHeight: "66px",
+            letterSpacing: "-0.01em",
+            fontWeight: "400",
+          },
+        ],
+      },
       colors: {
         red: {
           700: "#ff0000",
