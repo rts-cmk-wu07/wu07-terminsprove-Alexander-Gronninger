84c15a67da8fac1086d1ecdb31d556016ec1b566
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2023-02-15 12:05:49 +0100

    make classCard link to class's details page

--- src/templates/ClassCard.js
+++ src/templates/ClassCard.js
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
 import ClassImage from "../components/ClassImage";
 import Rating from "../components/Rating";
 
-const ClassCard = ({ image, title, rating }) => {
+const ClassCard = ({ image, title, rating, classId }) => {
+  classId = 1;
   return (
     <>
       <div className="mr-4">
-        <ClassImage image="https://picsum.photos/200" />
-        <h3 className="text-small overflow-hidden max-w-[150px] max-h-[26px] truncate">
-          Lorem Ipsumssssssssssss
-        </h3>
+        <Link to={"/class/" + classId} className="h-fit block">
+          <ClassImage image="https://picsum.photos/200" />
+          <h3 className="text-small overflow-hidden max-w-[150px] max-h-[26px] truncate">
+            Lorem Ipsumssssssssssss
+          </h3>
+        </Link>
         <Rating rating={rating} />
       </div>
     </>
